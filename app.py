ca3a75cf152c333d34b86cd2c7304c9892fa940f
realhack <realhack@kali.home.com>
2026-03-15 10:19:59 +0000

    fix port for render

--- app.py
+++ app.py
@@ -48,5 +48,8 @@ def home():
     return render_template("index.html", form=form)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
